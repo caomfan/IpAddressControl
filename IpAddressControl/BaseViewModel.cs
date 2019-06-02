9857120dcf49f7a8fd7939373007846e2d5a3a8d
CMF <1176023110@qq.com>
2019-06-02 13:04:34 +0800

    1.提交项目

---
+++ IpAddressControl/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace IpAddressControl
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ IpAddressControlDemo/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using IpAddressControl;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpAddressControlDemo
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+
+        private IpAddressViewModel _IpAddressViewModel1;
+
+        public IpAddressViewModel IpAddressViewModel1
+        {
+            get { return _IpAddressViewModel1; }
+            set
+            {
+                _IpAddressViewModel1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private IpAddressViewModel _IpAddressViewModel2;
+
+        public IpAddressViewModel IpAddressViewModel2
+        {
+            get { return _IpAddressViewModel2; }
+            set
+            {
+                _IpAddressViewModel2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            IpAddressViewModel1 = new IpAddressViewModel();
+            IpAddressViewModel2 = new IpAddressViewModel();
+        }
+    }
+}
